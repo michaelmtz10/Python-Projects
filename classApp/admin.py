51786f6b57b65e6d3b35c1e585423f3a6d415a84
michaelmtz10 <85362903+michaelmtz10@users.noreply.github.com>
2021-09-01 14:54:51 -0700

    Delete classApp directory

--- classApp/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from .models import DjangoClasses
-# registering the class to the admin page
-admin.site.register(DjangoClasses)
--- classApp/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-# configuring created class
-class DjangoClassesConfig(AppConfig):
-    name = 'classApp'
